49ebf801be6f800a34f7a9744067f0418449e18e
caznidarsic <cznidar1@jh.edu>
2023-09-13 13:19:38 -0700

    Updated url to reference api hosted on EC2

--- src/components/ReservoirData.js
+++ src/components/ReservoirData.js
@@ -37,7 +37,8 @@ function ReservoirData(props) {
 
     useEffect(() => {
         // const url = `/dynamicapp/req/JSONDataServlet?Stations=${props.id}&SensorNums=15&dur_code=${(chartRange === '6 months' ? 'D' : 'M')}&${getDateRange()}`;
-        const url = `http://localhost:3000/resdata?stationId=${props.id}&span=2`
+        const url = `http://ec2-52-25-236-61.us-west-2.compute.amazonaws.com:3000/resdata?stationid=${props.id}&span=2`
+        console.log(url)
         axios.get(url)
             .then(response => {
                 // setData(cleanData(response.data));
